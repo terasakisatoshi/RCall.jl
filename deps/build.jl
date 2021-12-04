2e0ff139c6fc3a619d6e71a8861e7903e5058e36
SatoshiTerasaki <terasakisatoshi.math@gmail.com>
2021-12-04 19:52:47 +0900

    add 'r' instead of 'r-base'

--- deps/build.jl
+++ deps/build.jl
@@ -33,7 +33,7 @@ try
             conda_provided_r = true
             Conda.add_channel("r")
             #Conda.add("r-base>=3.4.0,<4") # greater than or equal to 3.4.0 AND strictly less than 4.0
-            conda.add("r-base")
+            conda.add("r")
             Rhome = joinpath(Conda.LIBDIR, "R")
             libR = locate_libR(Rhome)
         else
